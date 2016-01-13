c1c40abe1002ed15ea782cd0bb663624d99ba5e3
iluadd <ildozh@gmail.com>
2016-01-13 19:09:20 +0300

    new ver

--- gui_forms_logic/frame_builder.py
+++ gui_forms_logic/frame_builder.py
@@ -39,6 +39,12 @@ class LabelFrame(QtGui.QFrame):
             self.setStyleSheet("background-color:lightgrey;")
 
         self.med_title = QtGui.QLabel(unicode(mediator.label))
+
+        # TODO move to Qss
+        a_font = QtGui.QFont("Courier", 12)
+        a_font.setBold(True)
+
+        self.med_title.setFont(a_font)
         self.layout().addWidget(self.med_title)
 
 
@@ -56,6 +62,7 @@ class LabelFrame(QtGui.QFrame):
             self.layout().addWidget(widget)
 
 
+# TODO make base class for frame
 class RecFrame(QtGui.QFrame):
     """
         Frame for concrete record
@@ -72,8 +79,13 @@ class RecFrame(QtGui.QFrame):
         # Adjusting Frame bounding sizes
         self.setFrameStyle(QtGui.QFrame.Raised)
         self.setLayout(QtGui.QVBoxLayout())
+        #   Margins
         self.layout().setContentsMargins(2, 2, 2, 2)
         self.layout().setSpacing(0)
+        #   =========================
+
+        self.setObjectName('Ramka')
+        self.setStyleSheet('#Ramka{border: 1px solid grey;}')
 
         if COLOR_DEBUG:
             self.setStyleSheet("background-color:lightgreen;")
@@ -81,7 +93,9 @@ class RecFrame(QtGui.QFrame):
         self.fields_and_html_frame = QtGui.QFrame()
         self.field_and_html_layout = QtGui.QHBoxLayout()
 
+        #   Margins
         self.field_and_html_layout.setContentsMargins(2, 2, 2, 2)
+        #   =========================
         if COLOR_DEBUG:
             self.fields_and_html_frame.setStyleSheet("background-color:cyan;")
         # ----------------------------
@@ -91,19 +105,14 @@ class RecFrame(QtGui.QFrame):
         if mediator.get_HTML() != "":
             self.html_field = GrowingTextBrowser(self)
             self.html_field.setReadOnly(1)
-
             self.html_field.setParent(self)
-            # (((o))) Signal
-            # self.html_field.mouseDoubleClickEvent = self.show_buttons
 
-            self.html_field.setHtml(mediator.get_HTML())
-            # self.html_field.setHtml('Blah Blah')
+            self.sheet_text = mediator.get_HTML()
+            self.html_field.setHtml(self.sheet_text)
+
             self.field_and_html_layout.addWidget(self.html_field)
             self.html_field.setAlignment(QtCore.Qt.AlignTop)
 
-            # print self.html_field.parent()
-            # a = self.html_field.parent()
-            # print a.parent()
 
         self.fields_and_html_frame.setLayout(self.field_and_html_layout)
         self.layout().addWidget(self.fields_and_html_frame)
@@ -112,28 +121,37 @@ class RecFrame(QtGui.QFrame):
         self.fields_frame_layout = QtGui.QVBoxLayout()
         if COLOR_DEBUG:
             self.fields_frame.setStyleSheet("background-color:pink;")
-        # self.fields_frame_layout.setAlignment(QtCore.Qt.AlignTop)
-        # |label value |
-        # |label value |
-        self.fields_frame_layout.setSpacing(0)
+
+        #   Margins
+        self.fields_frame_layout.setAlignment(QtCore.Qt.AlignTop)
         self.fields_frame_layout.setContentsMargins(1, 1, 1, 1)
+        self.fields_frame_layout.setSpacing(0)
+        #   =========================
+
+        # |label value |
+        # |label value |
         self.fields_frame.setLayout(self.fields_frame_layout)
 
         for field_i in mediator.iter_fields():
             one_field = QtGui.QWidget()
             one_field_layout = QtGui.QHBoxLayout()
+            #   Margins
             one_field_layout.setSpacing(0)
             one_field_layout.setContentsMargins(1, 1, 1, 1)
+            #   =========================
             # |label value |
             one_field_label = QtGui.QLabel(unicode(field_i.field_repr))
             one_field_value = QtGui.QLineEdit(unicode(field_i.field_value))
             one_field_layout.addWidget(one_field_label)
             one_field_layout.addWidget(one_field_value)
 
+            one_field_label.setFont(QtGui.QFont("Helvetica", italic=True))
+
             one_field_label.setMaximumWidth(150)
             one_field_value.setMaximumWidth(150)
 
             one_field.setLayout(one_field_layout)
+
             if COLOR_DEBUG:
                 one_field.setStyleSheet("background-color:lightblue;")
 
@@ -141,13 +159,18 @@ class RecFrame(QtGui.QFrame):
             temp_layout = QtGui.QVBoxLayout()
             temp_widget.setLayout(temp_layout)
             temp_layout.addWidget(one_field)
+            #   Margins
             one_field_layout.setSpacing(0)
             one_field_layout.setContentsMargins(1, 1, 1, 1)
+            temp_layout.setSpacing(0)
+            temp_layout.setContentsMargins(2, 2, 2, 2)
+            #   =========================
+            if COLOR_DEBUG:
+                temp_widget.setStyleSheet("background-color:red;")
             one_field_layout.setAlignment(QtCore.Qt.AlignTop)
             self.fields_frame_layout.addWidget(temp_widget)
 
         self.field_and_html_layout.insertWidget(0, self.fields_frame)
-        # self.field_and_html_layout.setAlignment(QtCore.Qt.AlignTop)
 
         # buttons layout
         #                                      -
@@ -162,8 +185,10 @@ class RecFrame(QtGui.QFrame):
             self.button_hor_layout.addWidget(button)
             self.buttons_field_widget = QtGui.QWidget()
 
+            #   Margins
             self.button_hor_layout.setContentsMargins(1, 1, 1, 1)
             self.buttons_field_widget.setContentsMargins(1, 1, 1, 1)
+            #   =========================
             if COLOR_DEBUG:
                 self.buttons_field_widget.setStyleSheet("background-color:orange;")
             self.buttons_field_widget.setLayout(self.button_hor_layout)
@@ -178,11 +203,7 @@ class RecFrame(QtGui.QFrame):
             self.inside_widgets.append(ch_i)
 
         for ch_i in self.inside_widgets:
-            # colorize frame !
-            # ch_i.enterEvent = self.frame_highlight
-            # engage buttons!
             ch_i.mouseDoubleClickEvent = self.show_buttons
-
 
     @classmethod
     def clear_active_buttons(cls):
@@ -202,7 +223,7 @@ class RecFrame(QtGui.QFrame):
 
     # TODO Make this by Qss hover pseudo states
     def enterEvent(self, *args, **kwargs):
-        self.setStyleSheet("background-color:white;")
+        self.setStyleSheet("#Ramka{background-color:lightblue;border: 1px dashed grey;}")
 
     def leaveEvent(self, *args, **kwargs):
-        self.setStyleSheet("background-color:None;")
+        self.setStyleSheet("#Ramka{background-color:None;border: 1px solid grey;}")
