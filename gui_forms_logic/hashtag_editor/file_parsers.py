ecc9f83cf1dc8f2967c4d49e75d85905b1ad4fcf
vyakhorev <alexey.vyakhorev@gmail.com>
2016-01-17 21:10:18 +0300

    Added .pdf support for note editor (still buggy)

--- gui_forms_logic/hashtag_editor/file_parsers.py
+++ gui_forms_logic/hashtag_editor/file_parsers.py
@@ -116,8 +116,36 @@ def _tags_from_addresses(email_addresses):
 def pdf_file_to_html(file_path):
     # Nice idea for images:
     # http://www.danvk.org/2015/01/09/extracting-text-from-an-image-using-ocropus.html
-    import pdfminer
-    return "I do not yet know how to import PDF"
+    # FIXME: not working with russian characters
+    # from http://stackoverflow.com/questions/26748788/extraction-of-text-from-pdf-with-pdfminer-gives-multiple-copies/26766684#26766684
+    from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+    from pdfminer.converter import TextConverter
+    from pdfminer.layout import LAParams
+    from pdfminer.pdfpage import PDFPage
+    from cStringIO import StringIO
+
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    fp = file(file_path, 'rb')
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    password = ""
+    maxpages = 0
+    caching = True
+    pagenos=set()
+    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages,password=password,caching=caching, check_extractable=True):
+
+        interpreter.process_page(page)
+
+    text = retstr.getvalue()
+
+    fp.close()
+    device.close()
+    retstr.close()
+    return text
+
 
 ##################
 # .DOC .DOCX
